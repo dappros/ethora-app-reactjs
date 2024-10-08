82bca7f6fc6438302a4e75029601ccf08c0dafea
Borys <transkarpation@gmail.com>
2024-10-08 12:56:16 +0300

    check google errro

--- src/pages/AuthPage/GoogleButton.tsx
+++ src/pages/AuthPage/GoogleButton.tsx
@@ -8,64 +8,70 @@ import { actionAfterLogin } from "../../actions"
 import { useNavigate } from "react-router-dom"
 
 export const GoogleButton = () => {
-    const config = useAppStore.getState().currentApp
-    const navigate = useNavigate()
-    const onGoogleLogin = async () => {
-        console.log("google login")
-        const loginType = "google"
-        const {user, idToken, credential} = await getUserCredsFromGoogle()
+  const config = useAppStore.getState().currentApp
+  const navigate = useNavigate()
+  const onGoogleLogin = async () => {
+    try {
+      console.log("google login")
+      const loginType = "google"
+      const { user, idToken, credential } = await getUserCredsFromGoogle()
 
-        if (!user.providerData[0].email) {  
-            toast.error("Email not provided by Google")
-            return
-        }
+      if (!user.providerData[0].email) {
+        toast.error("Email not provided by Google")
+        return
+      }
 
-        const emailExist = await httpCheckEmailExist(user.providerData[0].email)
+      const emailExist = await httpCheckEmailExist(user.providerData[0].email)
 
-        if (emailExist.data.success) {
-          console.log("new registration")
-          try {
-            await httpRegisterSocial(
-              idToken ?? "",
-              credential?.accessToken ?? "",
-              "",
-              loginType
-            )
-          } catch (error) {
-            toast.error("Social registration failed")
-          }
-
-          httpLoginSocial(idToken ?? "", credential?.accessToken ?? "", loginType).then(async ({data}) => {
-            await actionAfterLogin(data)
-            navigate("/app/admin/apps")
-          })
-        } else {
-          console.log("existing user")
-          httpLoginSocial(
+      if (emailExist.data.success) {
+        console.log("new registration")
+        try {
+          await httpRegisterSocial(
             idToken ?? "",
-              credential?.accessToken ?? "",
-              loginType
-            ).then(async ({data}) => {
-              await actionAfterLogin(data)
-              navigate("/app/admin/apps")
-            })
+            credential?.accessToken ?? "",
+            "",
+            loginType
+          )
+        } catch (error) {
+          toast.error("Social registration failed")
         }
+
+        httpLoginSocial(idToken ?? "", credential?.accessToken ?? "", loginType).then(async ({ data }) => {
+          await actionAfterLogin(data)
+          navigate("/app/admin/apps")
+        })
+      } else {
+        console.log("existing user")
+        httpLoginSocial(
+          idToken ?? "",
+          credential?.accessToken ?? "",
+          loginType
+        ).then(async ({ data }) => {
+          await actionAfterLogin(data)
+          navigate("/app/admin/apps")
+        })
+      }
+
+    } catch (error) {
+      console.log("++ ", error)
     }
-    return (
-        <CustomButton
-        fullWidth
-        variant="outlined"
-        startIcon={<GoogleIcon />}
-        onClick={() => onGoogleLogin()}
-        style={{
-          borderColor: config?.primaryColor
-            ? config.primaryColor
-            : "#0052CD",
-    
-          color: config?.primaryColor ? config.primaryColor : "#0052CD",
-        }}
-      >
-            Continue with Google
-        </CustomButton>
-    )
+
+  }
+  return (
+    <CustomButton
+      fullWidth
+      variant="outlined"
+      startIcon={<GoogleIcon />}
+      onClick={() => onGoogleLogin()}
+      style={{
+        borderColor: config?.primaryColor
+          ? config.primaryColor
+          : "#0052CD",
+
+        color: config?.primaryColor ? config.primaryColor : "#0052CD",
+      }}
+    >
+      Continue with Google
+    </CustomButton>
+  )
 }
