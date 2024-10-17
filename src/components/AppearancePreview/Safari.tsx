937b18743794c7ebfb769f42b91fe1f4da9d1e15
Borys <transkarpation@gmail.com>
2024-10-17 12:23:20 +0300

    safary

--- src/components/AppearancePreview/Safari.tsx
+++ src/components/AppearancePreview/Safari.tsx
@@ -6,16 +6,19 @@ interface Props {
     sublogo: string
 }
 
-export function Safari({tagline, appLogo}: Props) {
+export function Safari({ tagline, appLogo }: Props) {
     return (
         <div className="safari-preview">
             <div className="wrap">
                 <div className="wrapwrap">
                     <div className="left">
-                        <div className="logo" style={{backgroundImage: `url('${appLogo}')`}}></div>
-                        <div className="slogan">
-                            {tagline}
+                        <div className="logo-slogan">
+                            <div className="logo" style={{ backgroundImage: `url('${appLogo}')` }}></div>
+                            <div className="slogan">
+                                {tagline}
+                            </div>
                         </div>
+
                     </div>
                     <div className="right">
                         <div className="form">
@@ -25,6 +28,12 @@ export function Safari({tagline, appLogo}: Props) {
                                 <div className="step"></div>
                                 <div className="step"></div>
                             </div>
+                            <div className="inputs">
+                                <input type="text" value="Gloria" disabled />
+                                <input type="text" disabled />
+                                <input type="text" className="full" disabled />
+                            </div>
+                            <button className="preview-primary-btn">Sign Up</button>
                         </div>
                     </div>
                 </div>
