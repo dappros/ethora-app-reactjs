d0da4d91a63cfdd4b081084518a59530967ee15a
Roman Leshchuh <roman.leshchuh@dappros.com>
2024-10-28 09:54:33 +0200

    updated chat ui

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -10,7 +10,13 @@ export function ChatPage() {
       <div className="app-content-header">
         <div className="app-content-header-title">Chats</div>
       </div>
-      <div className="app-content-body">
+      <div
+        className="app-content-body"
+        style={{
+          height: 'calc(100% - 56px)',
+          padding: 0,
+        }}
+      >
         <Outlet />
         <Chat
           config={{
@@ -19,14 +25,15 @@ export function ChatPage() {
               secondary: config?.primaryColor || '#fff',
             },
             disableMedia: true,
-            disableRooms: true,
           }}
           roomJID={
             '5f9a4603b2b5bbfa6b228b642127c56d03b778ad594c52b755e605c977303979@conference.xmpp.ethoradev.com'
           }
           MainComponentStyles={{
             color: 'white',
-            maxHeight: '768px',
+            maxHeight: 'calc(100%)',
+            height: 'calc(100%)',
+            borderRadius: 16,
           }}
         />
       </div>
