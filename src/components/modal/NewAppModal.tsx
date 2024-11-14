b075a890f60b3456a46d6c5c4fee82640915e5eb
Borys <transkarpation@gmail.com>
2024-11-14 08:16:08 +0200

    rm comments

--- src/components/modal/NewAppModal.tsx
+++ src/components/modal/NewAppModal.tsx
@@ -7,7 +7,6 @@ import { toast } from 'react-toastify';
 import { actionCreateApp } from '../../actions';
 import { Loading } from '../Loading';
 import { TextInput } from '../ui/TextInput';
-// import './NewAppModal.scss';
 
 interface Props {
   onClose: () => void;
@@ -30,13 +29,11 @@ export function NewAppModal({ onClose }: Props) {
   };
 
   return (
-    // new-app-modal
     <Dialog
       className="fixed inset-x-0 inset-y-0 z-50 flex justify-center items-center bg-black/50"
       open={true}
       onClose={() => {}}
     >
-      {/* inner */}
       <DialogPanel className="p-4 sm:p-8 bg-white rounded-3xl w-full max-w-[640px] m-8">
         <div className="relative mb-8">
           <div className="text-center font-varela text-2xl">
