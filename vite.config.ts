754e811ce88cb96ee14cfc5c6fe198594ba0c309
Borys <transkarpation@gmail.com>
2024-11-18 12:42:13 +0200

    web app

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  }
 });
