e8ea6abb645f3a0b01713a39c06f6eaf597d93fb
Borys <transkarpation@gmail.com>
2024-11-18 11:09:56 +0200

    chat page

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,3 +1,39 @@
-export default function Chat() {
-  return <div>Chat</div>;
+import { useAppStore } from "../store/useAppStore";
+import { Chat } from '@ethora/chat-component';
+
+export default function ChatPage() {
+  const config = useAppStore((s) => s.currentApp);
+  return (
+    <div className="grid grid-rows-[auto,_1fr] gap-4 h-full">
+      <div className="md:px-8 flex flex-col justify-between items-stretch md:items-center md:flex-row">
+        <div className="font-varela mb-4 text-[24px] md:mb-0 md:text-[34px] leading-none">
+          Chats
+        </div>
+      </div>
+      <div className="rounded-2xl bg-white p-4 h-full overflow-hidden">
+        <Chat
+          config={{
+            colors: {
+              primary: config?.primaryColor || '#fff',
+              secondary: '#fff',
+            },
+            // @ts-ignore
+            roomListStyles: {
+              color: config?.primaryColor,
+              maxHeight: 'calc(100%)',
+              height: 'calc(100%)',
+              borderRadius: '16px 0px 0px 16px',
+              maxWidth: 432,
+            },
+            chatRoomStyles: {
+              color: config?.primaryColor,
+              maxHeight: 'calc(100%)',
+              height: 'calc(100%)',
+              borderRadius: '0px 16px 16px 0px',
+            },
+          }}
+        />
+      </div>
+    </div>
+  )
 }
