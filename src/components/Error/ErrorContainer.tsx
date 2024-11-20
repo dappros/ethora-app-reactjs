797379e6f421d07f68c5c10c6557fafa0f8acf26
Dmytro Berberov <d.berberov93@gmail.com>
2024-11-20 13:18:39 +0100

    added back

--- src/components/Error/ErrorContainer.tsx
+++ src/components/Error/ErrorContainer.tsx
@@ -1,7 +1,8 @@
 import { ReactElement } from "react";
 import { Box, Container, Typography } from "@mui/material";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import CustomButton from "../../pages/AuthPage/Button";
+import { useNavigate } from "react-router-dom";
+import { IconArrowLeft } from "../Icons/IconArrowLeft";
 
 interface ErrorContainerProps {
   status: string;
@@ -18,6 +19,8 @@ export const ErrorContainer = (props: ErrorContainerProps): ReactElement => {
     image,
   } = props;
 
+  const navigate = useNavigate();
+
   return (
     <Container className="w-full h-screen flex flex-col md:flex-row items-center justify-center md:justify-around md:gap-10">
       <Box className="flex flex-col items-center text-center">
@@ -26,8 +29,9 @@ export const ErrorContainer = (props: ErrorContainerProps): ReactElement => {
         <Typography className="pb-8">{description}</Typography>
         <CustomButton
           variant="contained"
-          startIcon={<ArrowBackIcon />}
+          startIcon={<IconArrowLeft />}
           style={{padding: "2px 48px"}}
+          onClick={()=> navigate(-1)}
         >
           Go Back
         </CustomButton>
