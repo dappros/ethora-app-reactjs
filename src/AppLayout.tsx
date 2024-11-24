63183c99c5d99bb98423655e343882c527fd4d4b
Borys <transkarpation@gmail.com>
2024-11-24 11:25:04 +0200

    height

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -15,15 +15,15 @@ export default function AppLayout() {
     return <Navigate to="/login" replace></Navigate>;
   } else {
     return (
-      <div className="bg-gray-100 w-full min-h-screen flex justify-center">
-        <div className="min-h-screen max-w-[1920px] w-full md:p-4">
+      <div className="bg-gray-100 w-full flex justify-center">
+        <div className="h-screen max-w-[1920px] w-full md:p-4">
           {/* app content */}
-          <div className="h-full overflow-hidden grid grid-rows-[72px,_1fr] md:grid-rows-1 md:grid-cols-[80px,_1fr] md:gap-4">
+          <div className="h-full grid grid-rows-[72px,_1fr] md:grid-rows-1 md:grid-cols-[80px,_1fr] md:gap-4">
             {/* menu */}
             <AppMenu />
             {/* router content */}
-            <div className="h-full p-4 md:p-0">
-                <Outlet />
+            <div className="p-4 md:p-0">
+              <Outlet />
             </div>
           </div>
         </div>
