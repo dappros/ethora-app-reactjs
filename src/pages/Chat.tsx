b1c2f4c94f13b67c09895fd20ab52cb1a354f260
Roman Leshchuh <roman.leshchuh@dappros.com>
2024-11-26 10:35:38 +0200

    updated chat version,fixed relog

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -3,6 +3,8 @@ import { useAppStore } from '../store/useAppStore';
 
 export default function ChatPage() {
   const config = useAppStore((s) => s.currentApp);
+  const { currentUser } = useAppStore((s) => s);
+
   return (
     <div className="grid grid-rows-[auto,_1fr] gap-4 h-full abc">
       <div className="md:px-8 flex flex-col justify-between items-stretch md:items-center md:flex-row">
@@ -23,13 +25,17 @@ export default function ChatPage() {
               maxHeight: 'calc(100%)',
               height: 'calc(100%)',
               borderRadius: '16px 0px 0px 16px',
-              maxWidth: 432,
+              border: 'none',
             },
             chatRoomStyles: {
               color: config?.primaryColor,
               maxHeight: 'calc(100%)',
               height: 'calc(100%)',
               borderRadius: '0px 16px 16px 0px',
+            },
+            userLogin: {
+              enabled: true,
+              user: currentUser,
             },
           }}
         />
