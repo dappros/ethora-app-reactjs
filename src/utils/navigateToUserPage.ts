81a97962e408c5d4ca18bf6b746cf57dc730ee57
Dmytro Berberov <d.berberov93@gmail.com>
2025-05-15 10:04:03 +0200

    delete console and fix navigate

--- src/utils/navigateToUserPage.ts
+++ src/utils/navigateToUserPage.ts
@@ -1,9 +1,8 @@
 import { NavigateFunction } from "react-router-dom";
 
 export function navigateToUserPage(navigate: NavigateFunction, page: string | null | undefined) {
-  console.log("page" , page);
-  if(page === 'admin') {
-    return navigate("/admin");
+  if(page === 'login') {
+    return navigate("/login");
   }
 
   switch (page) {
