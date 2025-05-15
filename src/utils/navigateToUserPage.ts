d7ebb64a8d3f847c68388c06883c89dc47c65017
Dmytro Berberov <d.berberov93@gmail.com>
2025-05-15 09:57:36 +0200

    fix login navigate

--- src/utils/navigateToUserPage.ts
+++ src/utils/navigateToUserPage.ts
@@ -1,26 +1,22 @@
 import { NavigateFunction } from "react-router-dom";
 
 export function navigateToUserPage(navigate: NavigateFunction, page: string | null | undefined) {
-  console.log('page', page)
-  if(page === 'chats') {
-    return navigate("/app/chat");
+  console.log("page" , page);
+  if(page === 'admin') {
+    return navigate("/admin");
   }
-  if(page && page !== '/login') {
-    navigate(page);
-  } else {
-    navigate("/app/admin/apps")
+
+  switch (page) {
+    case 'chats': {
+      navigate("/app/chat");
+      break;
+    }
+    case 'profile': {
+      navigate("/app/profile");
+      break;
+    }
+    default: {
+      navigate("/app/admin/apps");
+    }
   }
-    // switch (page) {
-    //     case 'chats': {
-    //       navigate("/app/chat")
-    //       break
-    //     }
-    //     case 'profile': {
-    //       navigate("/app/profile")
-    //       break
-    //     }
-    //     default: {
-    //       navigate("/app/admin/apps")
-    //     }
-    //   }
 }
