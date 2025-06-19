a3204e65eb229839bfb0e864271505dbc7ed1cd8
Dmytro Berberov <d.berberov93@gmail.com>
2025-06-19 09:27:49 +0200

    add referrer utm params

--- src/hooks/useCaptureParams.tsx
+++ src/hooks/useCaptureParams.tsx
@@ -4,6 +4,15 @@ interface Params {
   [key: string]: string;
 }
 
+const searchEngines: { [key: string]: string } = {
+  'google.': 'Google',
+  'bing.': 'Bing',
+  'yahoo.': 'Yahoo',
+  'duckduckgo.': 'DuckDuckGo',
+  'yandex.': 'Yandex',
+  'baidu.': 'Baidu',
+};
+
 export const useCaptureParams = (paramNames: string[] = []) => {
   useEffect(() => {
     const hasCapturedParams = localStorage.getItem('hasCapturedParams');
@@ -14,18 +23,29 @@ export const useCaptureParams = (paramNames: string[] = []) => {
     const urlParams = new URLSearchParams(window.location.search);
     const params: Params = {};
 
-    const paramsToCapture = paramNames.length > 0 ? paramNames : Array.from(urlParams.keys());
+    const paramsToCapture =
+      paramNames.length > 0 ? paramNames : Array.from(urlParams.keys());
 
-    paramsToCapture.forEach(param => {
+    paramsToCapture.forEach((param) => {
       const value = urlParams.get(param);
       if (value) {
         params[param] = value;
       }
     });
 
+    if (Object.keys(params).length === 0 && document.referrer) {
+      const referrer = document.referrer;
+      const searchEngine = Object.entries(searchEngines).find(([key]) =>
+        referrer.includes(key)
+      );
+      if (searchEngine) {
+        params['utm_ref'] = searchEngine[1];
+      }
+    }
+
     if (Object.keys(params).length > 0) {
       localStorage.setItem('urlParams', JSON.stringify(params));
       localStorage.setItem('hasCapturedParams', 'true');
     }
   }, []);
-}; 
+};
