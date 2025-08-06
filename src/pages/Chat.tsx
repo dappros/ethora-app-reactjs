2d956c8dda050f585f684790f7d80fe0e6950c8a
Dmytro Berberov <d.berberov93@gmail.com>
2025-08-06 18:24:59 +0200

    add in chat demo notice

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,4 +1,5 @@
 import { Chat } from '@ethora/chat-component';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import React from 'react';
 import { httpTokens, refreshToken } from '../http';
 import { useAppStore } from '../store/useAppStore';
@@ -83,7 +84,13 @@ const MemoizedChat = React.memo(function ChatComponent({
 
 export default function ChatPage() {
   const config = useAppStore((s) => s.currentApp);
+  const isAdmin = useAppStore((s) => s.currentApp?.isAllowedNewAppCreate);
+
   const { currentUser } = useAppStore((s) => s);
+
+  const allowedDomains =
+    import.meta.env.VITE_APP_ALLOWED_DOMAINS?.split(',') || [];
+  const currentDomain = window.location.hostname;
 
   return (
     <div className="grid grid-rows-[auto,_1fr] gap-4 h-full abc">
@@ -91,6 +98,17 @@ export default function ChatPage() {
         <div className="font-varela mb-4 text-[24px] md:mb-0 md:text-[34px] leading-none">
           Chats
         </div>
+        {isAdmin && allowedDomains.includes(currentDomain) && (
+          <div className="flex flex-col items-center bg-yellow-100 px-4 py-2 text-sm border">
+            <p>This is demo server</p>
+            <p className="flex items-center gap-2">
+              <span>To test your own App, go to "Admin"</span>
+              <ArrowRightAltIcon /> <span>your App </span>
+              <ArrowRightAltIcon /> <span>"Publish"</span>
+            </p>
+          </div>
+        )}
+        <div />
       </div>
       <div className="rounded-2xl bg-white px-0 overflow-hidden">
         <MemoizedChat config={config} currentUser={currentUser} />
