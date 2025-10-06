9636339882a34869d624091b4a554f9a1cc46a2f
Dmytro Berberov <d.berberov93@gmail.com>
2025-10-06 17:10:36 +0200

    check ci/cd

--- src/pages/AdminApps.tsx
+++ src/pages/AdminApps.tsx
@@ -106,7 +106,7 @@ export default function AdminApps() {
             { key: 'totalRegistered', title: 'Users' },
             { key: 'totalSessions', title: 'Sessions' },
             { key: 'totalApiCalls', title: 'API' },
-            // { key: 'totalTokensCalls', title: 'AI' },
+            // { key: 'totalTokens', title: 'AI' },
             { key: 'totalFiles', title: 'Files' },
             { key: 'totalTransactions', title: 'Transactions' },
             { key: 'createdAt', title: 'Date' },
